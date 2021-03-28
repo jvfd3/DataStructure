409c80180ef3dd3b33b4663c8470fa4d918c2bb8
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-28 20:53:59 -0300

    Concatenate Done

--- Programas - Estruturas de Dados 1/Q2/conjunto.c
+++ Programas - Estruturas de Dados 1/Q2/conjunto.c
@@ -17,7 +17,6 @@
 
 */
 
-// ajudinha de Rivera
 typedef struct Node{
 	int intValue;
 	struct Node* next;
@@ -122,22 +121,38 @@ void	InsertElementsTest2	(MainN* mainPointer){						//		b)	Inserir elementos no 
 }
 void	RemoveLastElement	(MainN* mainPointer){						//		c)	Remover um elemento do conjunto.
 	
-	printf("Removed  element %d. List:\t", mainPointer->last->intValue);
-	free(mainPointer->last);
-	mainPointer->listLength--;
-	
-	int loopCounter;
-	N* nodePointer=mainPointer->first;
-	
-	for (loopCounter=1;loopCounter<mainPointer->listLength;loopCounter++){
-		nodePointer=nodePointer->next;
+	if(mainPointer->listLength>0){
+		
+		printf("Removed  element %d. List:\t", mainPointer->last->intValue);
+		free(mainPointer->last);
+		mainPointer->listLength--;
+		
+		int loopCounter;
+		N* nodePointer=mainPointer->first;
+		
+		for (loopCounter=1;loopCounter<mainPointer->listLength;loopCounter++){
+			nodePointer=nodePointer->next;
+		}
+		nodePointer->next=NULL;
+		mainPointer->last=nodePointer;
+				
+		ShowList(mainPointer);
+	//	ShowListPointers(mainPointer);
+	} else {
+		printf("Nothing to be removed.\n");
 	}
-	nodePointer->next=NULL;
-	mainPointer->last=nodePointer;
-			
-	ShowList(mainPointer);
-//	ShowListPointers(mainPointer);
-	
+}
+void	InsRemElementsTest1	(MainN* mainPointer){
+	InsertElement(mainPointer,111);
+	InsertElement(mainPointer,222);
+	InsertElement(mainPointer,333);
+	InsertElement(mainPointer,444);
+	RemoveLastElement(mainPointer);			//		c)	Remover um elemento no conjunto.
+	RemoveLastElement(mainPointer);			//		c)	Remover um elemento no conjunto.
+	InsertElement(mainPointer,222);
+	InsertElement(mainPointer,333);
+	InsertElement(mainPointer,444);
+	InsertElement(mainPointer,555);
 }
 void	RemoveElementsTest	(MainN* mainPointer){						//		c)	Remover  elementos do conjunto.
 	RemoveLastElement(mainPointer);			//		c)	Remover um elemento no conjunto.
@@ -193,7 +208,7 @@ void	SmallestValue		(MainN* mainPointer){						//		e)	Menor valor do conjunto.
 	}
 	printf("Smallest value from list: %d\n", smallest);
 }
-void	BiggestValue		(MainN* mainPointer){						//		e)	Menor valor do conjunto.
+void	BiggestValue		(MainN* mainPointer){						//		e.1)	Maior valor do conjunto.
 	N* nodePointer=mainPointer->first;
 	int loopCounter, biggest=nodePointer->intValue ;
 	for(loopCounter=0;loopCounter<mainPointer->listLength;loopCounter++){
@@ -210,27 +225,21 @@ void	BiggestValue		(MainN* mainPointer){						//		e)	Menor valor do conjunto.
 void	Concatenate			(MainN* mainPointer1, MainN* mainPointer2, MainN* mainPointer3){		//		f)	Uniao de dois conjuntos.
 	
 	int loopCounter;
+	N* nodePointer;
 	
-	N* nodePointer=mainPointer1->first;
-	
+	nodePointer=mainPointer1->first;
 	for(loopCounter=0;loopCounter<mainPointer1->listLength;loopCounter++){
-		
 		InsertElement(mainPointer3, nodePointer->intValue);
-		
 		nodePointer=nodePointer->next;
 	}
 	
 	nodePointer=mainPointer2->first;
-	
 	for(loopCounter=0;loopCounter<mainPointer2->listLength;loopCounter++){
-		
 		InsertElement(mainPointer3, nodePointer->intValue);
-		
 		nodePointer=nodePointer->next;
 	}
-	
 }
-void	CheckEquality	(MainN* mainPointer1, MainN* mainPointer2){			//		g)	Testar se os dois conjuntos sao iguais.
+void	CheckEquality		(MainN* mainPointer1, MainN* mainPointer2){	//		g)	Testar se os dois conjuntos sao iguais.
 	
 	if (mainPointer1->listLength==mainPointer2->listLength){
 		
@@ -248,7 +257,7 @@ void	CheckEquality	(MainN* mainPointer1, MainN* mainPointer2){			//		g)	Testar s
 		printf("Lists are not equal. Different lengths: (list1Length, list2Length)=(%d, %d)\n", mainPointer1->listLength, mainPointer2->listLength);
 	}
 }
-void	CheckEmpty		(MainN* mainPointer){				//		h)	Testar se o conjunto e vazio.
+void	CheckEmpty			(MainN* mainPointer){						//		h)	Testar se o conjunto e vazio.
 }
-void	Clean			(MainN* mainPointer){
+void	Clean				(MainN* mainPointer){
 }
--- Programas - Estruturas de Dados 1/Q2/main.c
+++ Programas - Estruturas de Dados 1/Q2/main.c
@@ -4,23 +4,30 @@
 
 
 void main(){
-	MainN* l1=CreateEmptyList();	//		a)	Criar uma lista vazia.
+/**/MainN* l1=CreateEmptyList();	//		a)	Criar uma lista vazia.
 	
-	InsertElement(l1,111);			//		b)	Inserir um elemento no conjunto.
-//	InsertRemoveElementsTest1(l1);			/*		Insert:	1) First;	2) Last	(OK);	3) Random;	4)In order;*/
-	RemoveLastElement(l1);
-			
-	CheckNumber(l1,333);			//		d)	Testar se um número pertence ao conjunto.
+/**/InsertElement(l1,111);			//		b)	Inserir um elemento no conjunto.
+	InsRemElementsTest1(l1);		/*		Insert:	1) First;	2) Last	(OK);	3) Random;	4)In order;*/
+/**/RemoveLastElement(l1);			//		c)	Remover um elemento do conjunto.
+
+/**/CheckNumber(l1,333);			//		d)	Testar se um número pertence ao conjunto.
 	CheckNumber(l1,444);			//		d)	Testar se um número pertence ao conjunto.
-	SmallestValue(l1);				//		e)	Menor valor do conjunto.
+	
+/**/SmallestValue(l1);				//		e)	Menor valor do conjunto.
 	// BiggestValue(l1);			//		e)	Menor valor do conjunto.
-	MainN* l2=CreateEmptyList();	//		a)	Criar uma lista vazia.
-	InsertElement(l2,111);			//		b)	Inserir um elemento no conjunto.
-	InsertElementsTest(l2);
+	
+	MainN* l2=CreateEmptyList();
+	InsertElementsTest2(l2);
 	MainN* l3=CreateEmptyList();
-	Concatenate(l1,l2,l3);				//		f)	União de dois conjuntos.
-//	CheckEquality(l1,l2);			//		g)	Testar se os dois conjuntos são iguais.
-//	CheckEmpty(l1);					//		h)	Testar se o conjunto é vazio.
+	ShowList(l1);
+	ShowList(l2);
+	ShowList(l3);
+/**/Concatenate(l1,l2,l3);				//		f)	União de dois conjuntos.
+	ShowList(l1);
+	ShowList(l2);
+	ShowList(l3);
+/**///	CheckEquality(l1,l2);			//		g)	Testar se os dois conjuntos são iguais.
+/**///	CheckEmpty(l1);					//		h)	Testar se o conjunto é vazio.
 //	Clean(l1);
 //	Clean(l2);
 
