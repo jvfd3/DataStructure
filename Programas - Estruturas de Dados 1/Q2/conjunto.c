671f487d546c6e3d66a3972c8063776aa1ed98d7
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-29 02:05:34 -0300

    Now Variations Mode became a void function
    The mess was thrown under the carpet.

--- Programas - Estruturas de Dados 1/Q2/conjunto.c
+++ Programas - Estruturas de Dados 1/Q2/conjunto.c
@@ -27,15 +27,6 @@ typedef struct MainNode{
 	N* last;
 }MainN;
 
-void RunList(MainN* mainPointer){
-	int loopCounter;
-	N* nodePointer=mainPointer->first;
-	for(loopCounter=0;loopCounter<mainPointer->listLength;loopCounter++){
-		
-		nodePointer=nodePointer->next;
-	}
-}
-
 MainN* 	CreateEmptyList(){												//		a)	Criar uma lista vazia.
 	MainN* pointer = (MainN*) malloc (sizeof(MainN));
 	pointer->first=NULL;
@@ -335,4 +326,49 @@ void	Clean				(MainN* mainPointer){
 	free(nodePointer);
 	free(lastNodePointer);
 	printf("Cleaned.\n");
+}
+
+void RunList(MainN* mainPointer){
+	int loopCounter;
+	N* nodePointer=mainPointer->first;
+	for(loopCounter=0;loopCounter<mainPointer->listLength;loopCounter++){
+		
+		nodePointer=nodePointer->next;
+	}
+}
+
+void Variations(){
+	MainN* l1=CreateEmptyList();		//		a)	Criar uma lista vazia.
+	InsertElement(l1,111);				//		b)	Inserir um elemento no conjunto.
+	InsRemElementsTest1(l1);			//		Insert:	1) First;	2) Last	(OK);	3) Random;	4)In order;
+	RemoveLastElement(l1);				//		c)	Remover um elemento do conjunto.
+	CheckNumber(l1,333);				//		d)	Testar se um número pertence ao conjunto.
+	CheckNumber(l1,444);				//		d)	Testar se um número pertence ao conjunto.
+	SmallestValue(l1);					//		e)	Menor valor do conjunto.
+	// BiggestValue(l1);				//		e)	Menor valor do conjunto.
+	MainN* l2=CreateEmptyList();
+	InsertElementsTest2(l2);
+	MainN* l3=CreateEmptyList();
+	Concatenate(l1,l2,l3);				//		f)	União de dois conjuntos.
+	CheckEquality(l1,l2);				//		g)	Testar se os dois conjuntos são iguais.
+	MainN* l4=CreateEmptyList();
+	InsRemElementsTest1(l4);
+	ShowList(l4);
+	MainN* l5=CreateEmptyList();
+	InsRemElementsTest1(l5);
+	ShowList(l5);
+	CheckEquality(l4,l5);
+	RemoveLastElement(l4);
+	InsertElement(l4,888);
+	ShowList(l4);
+	CheckEquality(l4,l5);
+	CheckEmpty(l1);					//		h)	Testar se o conjunto é vazio.
+	MainN* l6=CreateEmptyList();
+	CheckEmpty(l6);	
+	Clean(l1);
+	Clean(l2);
+	Clean(l3);
+	Clean(l4);
+	Clean(l5);
+	Clean(l6);
 }
