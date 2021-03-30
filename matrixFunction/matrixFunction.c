bc409362c0f26173042062e5f31afc0fcdfdc42e
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-30 20:45:30 -0300

    Simple square matrix
    Trying to use Matrices inside Functions

---
+++ matrixFunction/matrixFunction.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int size=4;
+	int m[size][size];
+	int i,j,aux=0;
+	for (i=0;i<size;i++){
+		for (j=0;j<size;j++){
+			m[i][j]=aux;
+			aux++;
+		}
+	}
+	for (i=0;i<size;i++){
+		for (j=0;j<size;j++){
+			printf("%d\t",m[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+}
