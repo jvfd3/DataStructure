f752563165ef73750c83bd6f4601feea302b6e38
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-30 21:41:43 -0300

    Not many advances, let's try with vectors first
    Could see the type of the arguments sent, but not comprehend it right.

--- matrixFunction/matrixFunction.c
+++ matrixFunction/matrixFunction.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void printMatrix1(int m){
+	printf("%d",m);
+}
+
 int main(){
 		
 	int size=4;
 	int m[size][size];
+	int n[4][4];
 	int i,j,aux=0;
 	for (i=0;i<size;i++){
 		for (j=0;j<size;j++){
@@ -19,5 +24,16 @@ int main(){
 		printf("\n");
 	}
 	
-	
+	printf("Printing Function1>\n");
+	printMatrix1(m);
+	printMatrix1(&m);
+	printMatrix1(n);
+	printMatrix1(&n);
+	printf("\n<");
+	printf("Printing Function>\n");
+	printMatrix2(m);
+	printMatrix2(&m);
+	printMatrix2(n);
+	printMatrix2(&n);
+	printf("\n<");
 }
