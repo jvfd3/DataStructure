c318837b6520074181560a20a5978b5eb261d5fd
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-31 19:25:34 -0300

    Creating a function to measure time in C
    I was testing two different expressions. the second one was more compact

--- Lists/LE1/Q6 - Matrix time and Operations/Q6 - Matrix time and Operations.c
+++ Lists/LE1/Q6 - Matrix time and Operations/Q6 - Matrix time and Operations.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*	//Time example
+/*
 	clock_t fullTimer, sumTimer, multiplicationTimer;
 	double fullTimeTaken, sumTimeTaken, multiplicationTimeTaken;
     fullTimer = clock();
@@ -11,6 +11,7 @@
     fullTimeTaken = ((double)fullTimer)/CLOCKS_PER_SEC; // in seconds
     printf("Sum took %f seconds to execute.\n", time_taken);
 */
+
 
 /*
 	Considerando os conceitos de eficiência de algoritmos realize as seguintes tarefas:
@@ -57,29 +58,50 @@ void matrixMultiplication	(int size, int a[size][size], int b[size][size], int c
 		}
 	}
 }
+void printTimer				(clock_t time){
+	double timeTaken = ((double)time)/CLOCKS_PER_SEC;
+    printf("func 1 Time taken: %.4f ms.\n", timeTaken*1000);
+}
+void printTimer1				(clock_t time){
+    printf("func 2 Time taken: %.4f ms.\n", (double)time*1000/CLOCKS_PER_SEC);
+}
 
 int main(){
 
+	clock_t time;
+	time = clock();
 	int size=3;
+	double timeTaken;
+	
 	
 	printf("Matrix A:\n");
 	int a[size][size];
-	createMatrix(size,a);	//	Creating Matrix A
+	createMatrix(size,a);		//	Creating Matrix A
 	printMatrix(size,a);		//	Printing Matrix A
+	printTimer(clock()-time);
+//	
+//   
+//	double timeTaken = ((double)(clock()-time))/CLOCKS_PER_SEC;
+//    printf("%f\n", timeTaken);
 	
 	printf("Matrix 	B:\n");
 	int b[size][size];
-	createMatrix(size,b);	//	Creating Matrix B
+	createMatrix(size,b);		//	Creating Matrix B
 	printMatrix(size,b);		//	Printing Matrix B
 
 	printf("Sum Matrix:\n");
 	int c[size][size];
-	matrixSum(size,a,b,c);	//	Adding Matrixes A and B to C
+	matrixSum(size,a,b,c);		//	Adding Matrixes A and B to C
 	printMatrix(size,c);		//	Printing Sum Matrix
 		
 	printf("Multiplication Matrix:\n");
 	int d[size][size];
 	matrixMultiplication(size,a,b,d);	//	Multiplicating Matrixes A and B to D
 	printMatrix(size,d);				//	Printing Multiplication Matrix
-		
+	
+	printTimer(clock()-time);
+	printTimer1(clock()-time);
+	time=clock()-time;
+    timeTaken = ((double)time)/CLOCKS_PER_SEC;
+    printf("main Sum %f.\n", timeTaken*1000);
 }
