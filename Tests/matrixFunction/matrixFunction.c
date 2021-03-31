1509134bda8b78f7b12ea79ddcfec0b11e191ab7
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-03-30 22:01:34 -0300

    Success!!!
    🎉🎊✨🎇🎆

--- Tests/matrixFunction/matrixFunction.c
+++ Tests/matrixFunction/matrixFunction.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void printMatrix1(int m){
-	printf("%d",m);
+void printMatrix(int size, int a[size][size]){
+	printf("%d\n",a);
+	int i,j;
+	for (i=0;i<size;i++){
+		for (j=0;j<size;j++){
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+void changeMatrix(int size, int a[size][size]){
+	int i,j,aux=size*size;
+	
+	for (i=0;i<size;i++){
+		for (j=0;j<size;j++){
+			a[i][j]=aux;
+			aux--;
+		}
+	}
 }
 
 int main(){
 		
 	int size=4;
 	int m[size][size];
-	int n[4][4];
 	int i,j,aux=0;
 	for (i=0;i<size;i++){
 		for (j=0;j<size;j++){
@@ -25,15 +41,12 @@ int main(){
 	}
 	
 	printf("Printing Function1>\n");
-	printMatrix1(m);
-	printMatrix1(&m);
-	printMatrix1(n);
-	printMatrix1(&n);
+	printMatrix(size,m);
 	printf("\n<");
-	printf("Printing Function>\n");
-	printMatrix2(m);
-	printMatrix2(&m);
-	printMatrix2(n);
-	printMatrix2(&n);
+	printf("Printing Function2>\n");
+	changeMatrix(size,m);
+	printf("\n<");
+	printf("Printing Function3>\n");
+	printMatrix(size,m);
 	printf("\n<");
 }
