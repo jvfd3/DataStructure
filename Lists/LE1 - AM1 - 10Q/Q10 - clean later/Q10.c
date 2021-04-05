a054410fd43c8b6d43aa114f4aa5412f861ef4c8
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-04-05 20:33:49 -0300

    Q9 and Q10 done

--- Lists/LE1 - AM1 - 10Q/Q10 - clean later/Q10.c
+++ Lists/LE1 - AM1 - 10Q/Q10 - clean later/Q10.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+/*
 
+	10
+	A combinação de n objetos tomados k de cada vez,
+	pode ser calculada de maneira recursiva usando a seguinte formula de recorrência:
+ 
+	C(n, k){
+	    if ((k==0) || (n==k)){
+	        return 1;
+		} else if (n>k && k>0) {
+			return C(n-1,k)+C(n-1,k-1);
+		}      
+	}
+
+	Esta fórmula pode ser usada para calcular o número de combinações possíveis, k de n objetos.
+	Implemente um algoritmo recursivo para calcular a combinação de n objetos tomados k de cada vez.
+	Verifique o seu algoritmo, para C(49,6) que corresponde a um total de 13.983.816 combinações diferentes.
+
+
+*/
 int C(int n, int k){
     if (k==0 || n==k){
         return 1;
@@ -9,15 +27,18 @@ int C(int n, int k){
 	}      
 }
 
+int branchLessC(int n, int k){
+	return (k==0 || n==k) + (n>k && k>0)*(C(n-1,k)+C(n-1,k-1));    
+}
+
 int main() {
-	int n=49;
-	int k=6;
+	// QUESTÃO
 	
-//	printf("Por favor, insira o numero de combinacoes:");
-//	scanf("%d",&k);
-//	printf("\nPor favor, insira o numero de objetos:");
-//	scanf("%d",&n);
+	printf("C(49,6)= %d\n",C(49,6));
 	
-	int c=C(n,k);
-	printf("\nO numero de combinacoes possiveis %d de %d:%d",n,k,c);
+	//	JV
+		
+//	int n=49, k=6;
+//	printf("\nO numero de combinacoes possiveis %d de %d:%d",n,k,C(n,k));
+//	printf("\nO numero de combinacoes possiveis %d de %d:%d",n,k,branchLessC(n,k));
 }
