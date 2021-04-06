962fa76663f1b879579bea786fa34d75d261389b
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-04-05 21:26:36 -0300

    Q4 will be harder than expected

--- Lists/LE1 - AM1 - 10Q/Q4 - Client/Q4 - Client - main.c
+++ Lists/LE1 - AM1 - 10Q/Q4 - Client/Q4 - Client - main.c
@@ -1,13 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <String.h>
 /*
 	4.	Os dados relativos aos clientes de uma empresa estão armazenados em um arquivo.
-	Para cada cliente são registrados um código, nome, o endereço, o telefone,
+	Para cada cliente são registrados um 
+	
+	código, nome, o endereço, o telefone,
 	a data em que fez sua primeira compra na empresa,
 	a data da última compra e o valor da última compra.
+	
 	Especifique o TAD Clientes para armazenar
 	os dados dos clientes e as operações necessárias
 	para inserir, consultar e excluir esses dados.
 	Implemente uma aplicação que utilize o tipo Clientes.
 */
+
+
+typedef struct Clientes{
+	struct client *c;
+	struct ClientList *next;
+}ClientList;
+
+typedef struct MainClientes{
+	int listLength;
+	ClientList* first;
+	ClientList* last;
+}MainClientList;
+
+typedef struct Cliente {
+	char *codigo;
+	char *nome;
+	char *endereco;
+	char *telefone;
+	char *dataPrimeiraCompra;
+	char *dataUltimaCompra;
+	char *valorUltimaCompra;	
+}Client;
+
+
 int main(){
 	
 }
