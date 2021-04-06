627532d514b7d463ac768ba9741a0f745eb45e89
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-04-05 21:27:47 -0300

    opsie, no "ó", "ç" and "ú"

--- Lists/LE1 - AM1 - 10Q/Q4 - Client/Q4 - Client - main.c
+++ Lists/LE1 - AM1 - 10Q/Q4 - Client/Q4 - Client - main.c
@@ -5,9 +5,9 @@
 	4.	Os dados relativos aos clientes de uma empresa estão armazenados em um arquivo.
 	Para cada cliente são registrados um 
 	
-	código, nome, o endereço, o telefone,
+	codigo, nome, o endereco, o telefone,
 	a data em que fez sua primeira compra na empresa,
-	a data da última compra e o valor da última compra.
+	a data da última compra e o valor da ultima compra.
 	
 	Especifique o TAD Clientes para armazenar
 	os dados dos clientes e as operações necessárias
