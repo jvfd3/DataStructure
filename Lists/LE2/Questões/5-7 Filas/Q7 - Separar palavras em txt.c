7e102869ef18c93946c6e90c37f283da7de2e948
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-06-13 16:43:08 -0300

    Starting to write the algorithm

--- Lists/LE2/Questões/5-7 Filas/Q7 - Separar palavras em txt.c
+++ Lists/LE2/Questões/5-7 Filas/Q7 - Separar palavras em txt.c
@@ -7,13 +7,21 @@
 
 /*  Q7 - Separar palavras em txt
 
-Escreva um programa em C que classifique as palavras em um texto, disponivel na forma
+Escreva um programa em C
+que classifique as palavras em um texto, disponivel na forma
 de arquivo, em categorias de acordo com a letra inicial de cada palavra.
 As palavras iniciadas com a mesma letra deverao ser colocadas em uma mesma fila.
 Apos a classificacao, exiba o numero de palavras existentes em cada categoria.
 
 */
 
+/*  Ideia para o algoritmo:
+
+ler um arquivo:
+
+
+
+*/
 int main () {
     
 }
