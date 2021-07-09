0aae6066dc4689e5f733140a3abc3472ab485364
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-07-09 12:15:37 -0300

    removed ~ from Questões and ´ from Códigos

--- Lists/LE3/Questões LE3/Q3 -/Q3.c
+++ Lists/LE3/Questões LE3/Q3 -/Q3.c
@@ -4,6 +4,9 @@
 #include <String.h>
 
 #include "../TAD_BinaryTreeSearch/JV/Compacted/BST_ADT.h"
+// I can't import Queue 
+#include "../../LE2/Questoes/5-7 Filas/FilaJV/FilaJV.h"
+// #include "..\..\..\LE2\Questões\5-7 Filas\FilaJV\FilaJV.h"
 
 /* Q3 - 
 Escreva um programa em C,
@@ -35,6 +38,75 @@ Utilize o texto da sua escolha, com pelo menos 2 linhas, para testar o programa.
 
 */
 
+typedef struct table {
+  char* char1;
+  char* code; //not really sure if code is needed here
+  int*  frequency;
+} TABLE;
+
+
+
+/*  ==================== comparePhoneNumber ====================  
+      Compare two pointers and return low, equal, high.
+      Pre user1 and user2 are valid pointers to the struct USER
+      Post return low (-1), equal (0), or high (+1)
+*/
+int huffCompare (void* user1, void* user2) {
+/*  review it later
+  This part is ugly but what it does is:
+  It gets two dataPointers that are actually pointers to the struct USER, so they cast it to be
+  a pointer to USER type, and then gets the pointer for names, casts it into a pointer to int and then
+  grab its internal value
+*/
+// is this * out of everything really needed?
+int something;
+  char* name1 = something;
+  char* name2 = something;
+  return strcmp(name1,name2);
+}
+
+/*  ====================== printUserBST ======================  
+  Print Name and PhoneNumber from BST.
+      Pre  user is a pointer to an USER struct
+      Post "Name: Name_of_USER \t Phone Number: phone_number_of_the_user\n"
+*/
+void huffPrint (void* user) {
+  //Not sure if name will need the extra *
+  int something;
+  int   phone = something;
+  char* name  =   something;
+
+  printf("Name: %s \t Phone Number: %d\n", name, phone);
+  return; //NECESSARY? I don't think so
+}
+
+int createHuffmanTree () {
+  return 0;
+}
+
+int createHuffNode () {
+  return 0;
+}
+
+int createHuffStruct () {
+  return 0;
+
+}
+
+int _huffRecursive () {
+  return 0;
+
+}
+
+void q3 () {
+  int choice = 1;
+  char* str = selectString(choice);
+  QUEUE* queue = characterCount(str);
+  orderQueue(queue);
+  BST_TREE* huffmanTree = BST_Create(huffCompare);
+  createHuffmanTree (huffmanTree, queue);
+}
+
 int main () {
-    
+  q3();
 }
